ba3b46daeadbf656d4f9c4689aa6c41959827fba
Naju <naju.equipe@gmail.com>
2026-05-14 21:04:02 -0300

    Fix: Express 5 catch-all route syntax

--- server.ts
+++ server.ts
@@ -149,7 +149,7 @@ async function startServer() {
   } else {
     const distPath = path.join(__dirname, "dist");
     app.use(express.static(distPath));
-    app.get("*", (req, res) => {
+    app.get("(.*)", (req, res) => {
       res.sendFile(path.join(distPath, "index.html"));
     });
   }
